527004ffd64a9258229df6d60338973b101cc522
userzerone <kishorejat@hotmail.com>
2019-04-22 10:25:59 +0530

    initial Setup

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello there'),
+        ),
+        body: Center(
+          child: Text("whats up"),
+        ),
+      ),
+    );
+  }
+}
